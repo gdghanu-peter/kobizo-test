79e6a69b409e4bff92d861e55605fa3006fcf597
Peter <gdschanu.vietpt0905@gmail.com>
2024-11-13 15:46:45 +0700

    fix/remove-unuse-import

--- pages/index.vue
+++ pages/index.vue
@@ -2,48 +2,46 @@
   <Post @change-page="onPageChange"></Post>
 </template>
 <script setup lang="ts">
-  import type { PageState } from "primevue/paginator";
-  
-  const queryClient = useQueryClient();
-  const postsStore = usePostsStore();
-  
-  const page = computed(() => {
-    return postsStore.currentPage;
-  });
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts", page.value],
-    queryFn: () => fetchPosts(page.value),
-  });
-  
-  watch(data, (newData) => {
-    if (newData) {
-      postsStore.setPosts(newData);
-    }
-  });
-  
-  watch(isError, (errorState) => {
-      postsStore.setIsError(errorState);
-  });
-  watch(isLoading, (loadingState) => {
-      postsStore.setIsLoading(loadingState);
-  });
-  watch(error, (newError) => {
-    if (newError) {
-      postsStore.setError(newError);
-    }
-  });
-  const onPageChange = (newPage: number) => {
-    postsStore.setPage(newPage + 1);
-    queryClient.invalidateQueries({ queryKey: ["posts"] });
-  };
-  
-  useHead({
-    title: "The Blog",
-    meta: [
-      { name: "description", content: "Blog description Seo" },
-      { name: "keywords", content: "Blog" },
-      { property: "og:title", content: "Demo title" },
-      { property: "og:description", content: "Demo description" },
-    ],
-  });
-  </script>
+const queryClient = useQueryClient();
+const postsStore = usePostsStore();
+
+const page = computed(() => {
+  return postsStore.currentPage;
+});
+const { data, isLoading, isError, error } = useQuery({
+  queryKey: ["posts", page.value],
+  queryFn: () => fetchPosts(page.value),
+});
+
+watch(data, (newData) => {
+  if (newData) {
+    postsStore.setPosts(newData);
+  }
+});
+
+watch(isError, (errorState) => {
+  postsStore.setIsError(errorState);
+});
+watch(isLoading, (loadingState) => {
+  postsStore.setIsLoading(loadingState);
+});
+watch(error, (newError) => {
+  if (newError) {
+    postsStore.setError(newError);
+  }
+});
+const onPageChange = (newPage: number) => {
+  postsStore.setPage(newPage + 1);
+  queryClient.invalidateQueries({ queryKey: ["posts"] });
+};
+
+useHead({
+  title: "The Blog",
+  meta: [
+    { name: "description", content: "Blog description Seo" },
+    { name: "keywords", content: "Blog" },
+    { property: "og:title", content: "Demo title" },
+    { property: "og:description", content: "Demo description" },
+  ],
+});
+</script>
