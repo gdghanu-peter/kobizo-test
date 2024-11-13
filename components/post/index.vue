9910f37ff313185a34b168ec54e22c797f9112bc
Peter <gdschanu.vietpt0905@gmail.com>
2024-11-13 15:45:36 +0700

    feat/clean-code

--- components/post/index.vue
+++ components/post/index.vue
@@ -51,49 +51,17 @@
     </div>
   </template>
   <script setup lang="ts">
-  import type { PageState } from "primevue/paginator";
-  
-  const queryClient = useQueryClient();
+import type { PageState } from 'primevue/paginator';
+
   const postsStore = usePostsStore();
+
   
-  const page = computed(() => {
-    return postsStore.currentPage;
-  });
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts", page.value],
-    queryFn: () => fetchPosts(page.value),
-  });
-  
-  watch(data, (newData) => {
-    if (newData) {
-      postsStore.setPosts(newData);
-    }
-  });
-  
-  watch(isError, (errorState) => {
-      postsStore.setIsError(errorState);
-  });
-  watch(isLoading, (loadingState) => {
-      postsStore.setIsLoading(loadingState);
-  });
-  watch(error, (newError) => {
-    if (newError) {
-      postsStore.setError(newError);
-    }
-  });
-  const onPageChange = (event: PageState) => {
-    postsStore.setPage(event.page + 1);
-    queryClient.invalidateQueries({ queryKey: ["posts"] });
-  };
-  
-  useHead({
-    title: "The Blog",
-    meta: [
-      { name: "description", content: "Blog description Seo" },
-      { name: "keywords", content: "Blog" },
-      { property: "og:title", content: "Demo title" },
-      { property: "og:description", content: "Demo description" },
-    ],
-  });
+  const emit = defineEmits<{
+  (e: 'changePage', page: number): void
+}>()
+
+const onPageChange =(event: PageState) =>{
+    emit('changePage', event.page)
+}
   </script>
   
--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,3 +1,60 @@
 <template>
-  <PostDetail></PostDetail>
+  <PostDetail :post-id="postId"></PostDetail>
 </template>
+<script setup lang="ts">
+import { usePostDetailStore } from "~/stores/postDetailStore";
+import type { Post } from "~/types/post";
+
+const route = useRoute();
+const postId = Number(route.params.id);
+const postDetailStore = usePostDetailStore();
+const { data, isLoading, isError, error } = useQuery({
+  queryKey: ["postId", postId],
+  queryFn: () => fetchPostDetail(postId),
+  enabled: !!postId,
+});
+
+watch(data, (newData) => {
+  if (newData) {
+    postDetailStore.setPostDetail(newData);
+    setSeoMetadata(newData);
+  }
+});
+watch(isError, (errorState) => {
+  postDetailStore.setIsError(errorState);
+});
+watch(isLoading, (loadingState) => {
+  postDetailStore.setIsLoading(loadingState);
+});
+watch(error, (newError) => {
+  if (newError) {
+    postDetailStore.setError(newError);
+  }
+});
+
+const setSeoMetadata = (post: Post) => {
+  useHead({
+    title: post.title,
+    meta: [
+      { name: "description", content: post.body.slice(0, 150) + "..." },
+      {
+        name: "keywords",
+        content:
+          post.title +
+          ", blog, " +
+          post.created_by.first_name +
+          " " +
+          post.created_by.last_name,
+      },
+      {
+        name: "author",
+        content: `${post.created_by.first_name} ${post.created_by.last_name}`,
+      },
+      { property: "og:title", content: post.title },
+      { property: "og:description", content: post.body.slice(0, 150) + "..." },
+      { property: "og:type", content: "article" },
+      { property: "og:image", content: post.created_by.avatar },
+    ],
+  });
+};
+</script>
