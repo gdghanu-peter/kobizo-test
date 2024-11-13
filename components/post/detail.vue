5a4a05344c4d825baa1c81879965f6e5e2c1d297
Peter <gdschanu.vietpt0905@gmail.com>
2024-11-13 15:34:13 +0700

    feat/move-file-to-components

---
+++ components/post/detail.vue
@@ -0,0 +1,112 @@
+<template>
+    <Breadcrumb class="text-gray-500 text-xl" :model="breadcrumbItems" >
+      <template #item="{ item }">
+        <router-link v-slot="{ href }" :to="item.route" custom>
+          <NuxtLink :to="href">
+            <span class="text-primary font-semibold hover:text-green-500">{{ item.label }}</span>
+          </NuxtLink>
+        </router-link>
+      </template>
+    </Breadcrumb>
+    <div class="p-4" v-if="!postDetailStore.isLoading && postDetailStore.postDetail">
+      <p class="text-gray-500 mb-1">
+        {{ formatDate(postDetailStore.postDetail.created_at) }}
+      </p>
+      <h1 class="text-3xl sm:text-4xl font-bold text-gray-900 mb-6">
+        {{ postDetailStore.postDetail.title }}
+      </h1>
+      <div class="flex mb-6 items-center">
+        <Avatar
+          :image="postDetailStore.postDetail.created_by.avatar"
+          class="mr-2"
+          shape="circle"
+        />
+        <div class="font-medium">
+          <p>
+            {{ postDetailStore.postDetail.created_by.first_name }}
+            {{ postDetailStore.postDetail.created_by.last_name }}
+          </p>
+          <p class="text-gray-500 leading-4">
+            @{{ postDetailStore.postDetail.created_by.username }}
+          </p>
+        </div>
+      </div>
+      <div v-html="postDetailStore.postDetail.body"></div>
+    </div>
+    <div v-else-if="!postDetailStore.isError" class="flex justify-center h-[80vh] items-center">
+      <ProgressSpinner aria-label="Loading" />
+    </div>
+    <div class="text-3xl flex justify-center h-[80vh] items-center" v-else>
+      Fetch Data Fail <br> 
+      Message: {{ postDetailStore.error?.message }} <br>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { usePostDetailStore } from "~/stores/postDetailStore";
+  import type { Post } from "~/types/post";
+  
+  const route = useRoute();
+  const postId = Number(route.params.id);
+  const postDetailStore = usePostDetailStore();
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["postId", postId],
+    queryFn: () => fetchPostDetail(postId),
+    enabled: !!postId,
+  });
+  
+  const breadcrumbItems = computed(() => [
+    { label: "Home", route: "/" },
+    {
+      label: postDetailStore.postDetail?.title ?? "Loading title...",
+      route: `/post/${postId}`,
+    },
+  ]);
+  
+  watch(data, (newData) => {
+    if (newData) {
+      postDetailStore.setPostDetail(newData);
+      setSeoMetadata(newData);
+    }
+  });
+  watch(isError, (errorState) => {
+      postDetailStore.setIsError(errorState);
+  
+  });
+  watch(isLoading, (loadingState) => {
+  
+      postDetailStore.setIsLoading(loadingState);
+  });
+  watch(error, (newError) => {
+    if (newError) {
+      postDetailStore.setError(newError);
+    }
+  });
+  
+  const setSeoMetadata = (post: Post) => {
+    useHead({
+      title: post.title,
+      meta: [
+        { name: "description", content: post.body.slice(0, 150) + "..." }, // Description of the post
+        {
+          name: "keywords",
+          content:
+            post.title +
+            ", blog, " +
+            post.created_by.first_name +
+            " " +
+            post.created_by.last_name,
+        },
+        {
+          name: "author",
+          content: `${post.created_by.first_name} ${post.created_by.last_name}`,
+        },
+        { property: "og:title", content: post.title },
+        { property: "og:description", content: post.body.slice(0, 150) + "..." },
+        { property: "og:type", content: "article" },
+        { property: "og:image", content: post.created_by.avatar },
+      ],
+    });
+  };
+  </script>
+  
---
+++ components/post/index.vue
@@ -0,0 +1,99 @@
+<template>
+    <h1
+      class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl mb-2 lg:mb-4"
+    >
+      The Blog
+    </h1>
+    <div class="mb-4 lg:mb-6 text-lg text-gray-500">
+      Read the latest news about all solutions, from framework announcements to
+      integration tutorials.
+    </div>
+    <div v-if="!postsStore.isLoading">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 items-center mb-4"
+      >
+        <div v-for="card in postsStore.posts" :key="card.id">
+          <NuxtLink :to="`/post/${card.id}`">
+            <Card class="max-w-[400px] overflow-hidden hover:bg-gray-50">
+              <template #title
+                ><h2 class="min-h-[60px]">{{ card.title }}</h2>
+              </template>
+              <template #content>
+                <p class="line-clamp-3" v-html="card.body"></p>
+              </template>
+              <template #footer>
+                <div class="flex justify-between items-center mt-2">
+                  <div>{{ formatDate(card.created_at) }}</div>
+                  <Avatar
+                    :image="card.created_by.avatar"
+                    class="mr-2"
+                    shape="circle"
+                  />
+                </div>
+              </template>
+            </Card>
+          </NuxtLink>
+        </div>
+      </div>
+      <Paginator
+        :rows="10"
+        :totalRecords="30"
+        :page="postsStore.currentPage - 1"
+        @page="onPageChange"
+      />
+    </div>
+    <div v-else-if="!postsStore.isError" class="flex justify-center h-[80vh] items-center">
+      <ProgressSpinner aria-label="Loading" />
+    </div>
+    <div class="text-3xl flex justify-center h-[80vh] items-center" v-else>
+      Fetch Data Fail <br> 
+      Message: {{ postsStore.error?.message }} <br>
+    </div>
+  </template>
+  <script setup lang="ts">
+  import type { PageState } from "primevue/paginator";
+  
+  const queryClient = useQueryClient();
+  const postsStore = usePostsStore();
+  
+  const page = computed(() => {
+    return postsStore.currentPage;
+  });
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts", page.value],
+    queryFn: () => fetchPosts(page.value),
+  });
+  
+  watch(data, (newData) => {
+    if (newData) {
+      postsStore.setPosts(newData);
+    }
+  });
+  
+  watch(isError, (errorState) => {
+      postsStore.setIsError(errorState);
+  });
+  watch(isLoading, (loadingState) => {
+      postsStore.setIsLoading(loadingState);
+  });
+  watch(error, (newError) => {
+    if (newError) {
+      postsStore.setError(newError);
+    }
+  });
+  const onPageChange = (event: PageState) => {
+    postsStore.setPage(event.page + 1);
+    queryClient.invalidateQueries({ queryKey: ["posts"] });
+  };
+  
+  useHead({
+    title: "The Blog",
+    meta: [
+      { name: "description", content: "Blog description Seo" },
+      { name: "keywords", content: "Blog" },
+      { property: "og:title", content: "Demo title" },
+      { property: "og:description", content: "Demo description" },
+    ],
+  });
+  </script>
+  
